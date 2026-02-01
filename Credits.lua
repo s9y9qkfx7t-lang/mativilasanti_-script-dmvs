35055fba043cb5fda2a1c0e38935276a1d13e28c
mativilasanti_ <s9y9qkfx7t@privaterelay.appleid.com>
2026-02-01 00:52:14 -0300

    Create Credits.lua

---
+++ Credits.lua
@@ -0,0 +1,8 @@
+-- mativilasanti_ pro player script - Créditos
+
+local creditsTab = Window:Tab({ Title = "Créditos", Icon = "info" })
+local sec = creditsTab:Section({ Title = "Creado por", Opened = true })
+sec:Paragraph({
+  Title = "Matias",
+  Desc = "@mativilasanti_ en Instagram - Pro player script 🔥"
+})
